6a06444c1ded14c6316d7dd76273d27be41f5d4b
tyler.steele <tyler.steele@libertymutual.com>
2019-09-06 09:59:59 -0400

    adding add command

---
+++ packages/cli/bin/commands/add/index.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+import commander from 'commander';
+import fs from 'fs-extra';
+import path from 'path';
+import { camelCase, set } from 'lodash';
+import logger from '../../../logger';
+import packageman from '../../../packageman';
+
+const versionRegex = /@[\^~><=]*\d(.\d)?(.\d)?/;
+let installTarget;
+commander
+	.arguments('<pkg>', 'Adds an npm module to the bmo dependencies. The package is installed if it does not exist already')
+	.option('as <alias>', 'Adds the package with an alias')
+	.action((pkg) => {
+		installTarget = pkg;
+	});
+
+commander.parse(process.argv);
+
+const run = async () => {
+	const pkgPath = path.resolve(process.cwd(), './package.json');
+	let pkg = require(pkgPath);
+	const version = installTarget.match(versionRegex) || '';
+	const pkgName = installTarget.replace(version, '');
+	logger.info(`Installing ${pkgName} ${version} to ${pkg.name}`);
+	console.log(pkg.dependencies);
+	if (!pkg.dependencies[pkgName]) {
+		await packageman.add(installTarget);
+		// refresh the package.json in case it was modified.
+		delete require.cache[require.resolve(pkgPath)];
+		pkg = require(pkgPath);
+	}
+	pkg.bmo = pkg.bmo || { modules: {} };
+	const parts = pkgName.replace('@', '').split('/').map(camelCase);
+	let moduleName = parts.length > 1 ? parts[1] : parts[0];
+	if (commander.as) {
+		moduleName = commander.as;
+	}
+	logger.info(`Adding ${pkgName} as ${moduleName}`);
+
+	set(pkg.bmo, `modules.${pkgName}`, moduleName);
+	fs.writeFileSync(pkgPath, JSON.stringify(pkg, 0, 2));
+};
+
+run();
---
+++ packages/cli/packageman.js
@@ -0,0 +1,53 @@
+import execa from 'execa';
+import fs from 'fs-extra';
+
+const defaultRegistry = 'default';
+const registries = [
+	'https://pi-artifactory.lmig.com/artifactory/api/npm/npm',
+	defaultRegistry
+];
+const yarn = 'yarn';
+const npm = 'npm';
+const install = 'install';
+const add = 'add';
+const packageManagers = [yarn, npm];
+const shell = true;
+const isYarn = fs.existsSync('yarn.lock');
+
+const installCmds = {
+	[yarn]: () => {
+		const cmd = execa(yarn, [install], { shell });
+		cmd.stdout.pipe(process.stdout);
+		return cmd;
+	},
+	[npm]: () => {
+		const cmd = execa(npm, [install], { shell });
+		cmd.stdout.pipe(process.stdout);
+		return cmd;
+	}
+};
+
+const addCommands = {
+	[yarn]: (pkg) => {
+		const cmd = execa(yarn, [add, pkg], { shell });
+		cmd.stdout.pipe(process.stdout);
+		return cmd;
+	},
+	[npm]: (pkg) => {
+		const cmd = execa(npm, [install, pkg], { shell });
+		cmd.stdout.pipe(process.stdout);
+		return cmd;
+	}
+};
+let pkgManager = isYarn ? yarn : npm;
+
+export default {
+	install: () => installCmds[pkgManager](),
+	add: (pkg) => addCommands[pkgManager](pkg),
+	use: (manager) => {
+		if (!packageManagers.includes(manager)) {
+			throw new Error(`Unknown package manager ${manager}`);
+		}
+		pkgManager = manager;
+	}
+};
