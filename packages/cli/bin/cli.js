9b803f32eba613f0d70b772b6c7264e670acb29f
RAJESH SUSAI <Rajesh.Susai@libertymutual.com>
2019-10-09 13:59:48 -0400

    pointing bmo-cli commands to right files, updating versions, and deleting yarn.lock files

--- packages/cli/bin/cli.js
+++ packages/cli/bin/cli.js
@@ -10,9 +10,9 @@ const run = async () => {
 	const ext = await extensions();
 	program
 		.version('0.1.0')
-		.command('start', 'start an application server in the current directory', { executableFile: `${__dirname}/bmo-create.js` })
+		.command('start', 'start an application server in the current directory', { executableFile: `${__dirname}/bmo-start.js` })
 		.command('create', 'creates a BMO http application', { executableFile: `${__dirname}/bmo-create.js` })
-		.command('add', 'adds a npm module to the bmo dependencies, and installs if it is not in the package.json', { executableFile: `${__dirname}/bmo-create.js` });
+		.command('add', 'adds a npm module to the bmo dependencies, and installs if it is not in the package.json', { executableFile: `${__dirname}/bmo-add.js` });
 
 	each(ext.commands, (cmd, key) => {
 		console.log(`Loading command ${key}`);
