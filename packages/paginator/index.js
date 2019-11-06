c2aeaef3bcb9c541c8f103ad3b66dfff49b6c021
Benjamin Reul <benjamin.reul@libertymutual.com>
2019-11-06 09:49:25 -0500

    updating paginator to allow query params

--- packages/paginator/index.js
+++ packages/paginator/index.js
@@ -1,26 +1,31 @@
 import clamp from 'lodash.clamp';
-const formatUrl = (host, resource, offset, limit) => `${host}${resource}?offset=${offset}&limit=${limit}`;
+
+const formatParams = (params) => Object.keys(params)
+	.reduce((acc, param) => acc.concat(`&${param}=${params[param]}`), '');
+
+const formatUrl = (host, resource, offset, limit, params) =>
+	`${host}${resource}?offset=${offset}&limit=${limit}${params}`;
 export default ({
 	config: {
 		server: {
 			host
 		}
 	}
-}) => ({ total, offset, limit, resource }) => {
+}) => ({ total, offset, limit, resource, params = {} }) => {
 	let next, prev, last;
-	const self = formatUrl(host, resource, offset, limit);
-	last = formatUrl(host, resource, total - limit, limit);
+	const queryParams = formatParams(params);
+	const self = formatUrl(host, resource, offset, limit, queryParams);
+	last = formatUrl(host, resource, total - limit, limit, queryParams);
 	if (total >= limit + offset) {
-		next = formatUrl(host, resource, offset + limit, limit);
+		next = formatUrl(host, resource, offset + limit, limit, queryParams);
 	}
 	if (offset - limit >= 0) {
-		prev = formatUrl(host, resource, clamp(offset - limit, 0, total), limit);
+		prev = formatUrl(host, resource, clamp(offset - limit, 0, total), limit, queryParams);
 	} else if (offset !== 0) {
-		prev = formatUrl(host, resource, 0, limit);
+		prev = formatUrl(host, resource, 0, limit, queryParams);
 	}
 
 	if (offset + limit >= total) {
 		last = undefined;
 	}
 	return { next, prev, self, last };
-};
