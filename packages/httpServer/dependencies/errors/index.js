2d78982dd756203f5461ea897b82f30a08a255d8
Benjamin Reul <benjamin.reul@libertymutual.com>
2019-11-08 14:50:30 -0500

    adding NotFound Error

--- packages/httpServer/dependencies/errors/index.js
+++ packages/httpServer/dependencies/errors/index.js
@@ -2,10 +2,13 @@ import ExtendableError from 'extendable-error';
 class Unauthorized extends ExtendableError {}
 class Validation extends ExtendableError {}
 class Unauthenticated extends ExtendableError {}
+class NotFound extends ExtendableError {}
+
 export default () => ({
 	Unauthorized,
 	Unauthenticated,
 	Validation,
+	NotFound,
 	// To avoid conflicts with global Error
 	ExtendableError
 });
