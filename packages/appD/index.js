bdace1af14cceeda1e858d76e8c985e723293cce
Benjamin Reul <benjamin.reul@libertymutual.com>
2019-11-08 14:09:43 -0500

    using NODE env for appd appName

--- packages/appD/index.js
+++ packages/appD/index.js
@@ -8,7 +8,7 @@ export default ({ config: { appDEnabled = false } }) => {
 			controllerSslEnabled: true,
 			accountName: process.env.APPDYNAMICS_AGENT_ACCOUNT_NAME,
 			accountAccessKey: process.env.APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY,
-			applicationName: `${process.env.APP_NAME}_${process.env.APP_ENV}`,
+			applicationName: `${process.env.APP_NAME}_${process.env.NODE_ENV}`,
 			tierName: `${process.env.APP_NAME}`,
 			nodeName: `${process.env.APP_NAME}`,
 			reuseNode: true,
