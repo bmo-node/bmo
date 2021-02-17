9017e2c468aa8339b387ee44621da339e95fb1e2
tyler.steele <tyler.steele@libertymutual.com>
2021-02-17 14:09:09 -0500

    few more major package updates. Fix commander in httpServer

--- packages/httpServer/dependencies/args/index.js
+++ packages/httpServer/dependencies/args/index.js
@@ -1,6 +1,7 @@
-export default ({ dependencies: { commander }}) => {
+export default async ({ dependencies: { commander }}) => {
   const collect = (value, previous) => previous.concat([ value ])
   commander
+    .storeOptionsAsProperties()
     .option('-s, --serve <folder>', 'Adds a folder to be served statically', collect, [])
     .allowUnknownOption()
   return commander.parse(process.argv)
