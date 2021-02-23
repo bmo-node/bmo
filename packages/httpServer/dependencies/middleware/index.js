729dd6708a4baf5ac69f572e90095d9727f41f0b
tyler.steele <tyler.steele@libertymutual.com>
2021-02-23 10:28:37 -0500

    allowing more granular configuration of helmet.

--- packages/httpServer/dependencies/middleware/index.js
+++ packages/httpServer/dependencies/middleware/index.js
@@ -1,23 +1,44 @@
 
 import bodyparser from 'koa-bodyparser'
-import helmet from 'koa-helmet'
+import kHelmet from 'koa-helmet'
+// Koa helmet does not have access to the default directives
+// so import its underlying dependency here and use it directly for the
+// default directives.
+import helmet from 'helmet'
+import { merge } from 'lodash'
 import errorMapper from './errorMapper'
 import logger from './logger'
 import errorHandler from './errorHandler'
+
 export default [
   // Already a module so no need to wrap it
   errorHandler,
   ({
     config: {
       server: {
+        mergeDefaultCspDirectives = true,
         helmet: helmetConfig = {
+          contentSecurityPolicy: {
+            directives: {}
+          },
           frameguard: {
             action: 'SAMEORIGIN'
           }
         }
       }
     }
-  }) => helmet(helmetConfig),
+  }) => {
+    let { contentSecurityPolicy } = helmetConfig.contentSecurityPolicy
+    if (mergeDefaultCspDirectives) {
+      contentSecurityPolicy = merge({}, contentSecurityPolicy, { directives: helmet.contentSecurityPolicy.getDefaultDirectives() })
+    }
+
+    const config = {
+      ...helmetConfig,
+      contentSecurityPolicy
+    }
+    return kHelmet(config)
+  },
   ({
     config: {
       server: {
