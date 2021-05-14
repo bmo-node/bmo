1db5cd38168196b3bd449e83291ae9882dad270b
tyler.steele <tyler.steele@libertymutual.com>
2021-05-14 11:17:04 -0400

    fix lint issue.

--- packages/cli/dependencies/commands/add/command.js
+++ packages/cli/dependencies/commands/add/command.js
@@ -7,6 +7,6 @@ export default ({
 }) => program.command('add <pkgName>')
   .option('--as <newName>', 'Add package with alias')
   .description('Adds an npm module to the bmo dependencies. The package is installed if it does not exist already')
-  .action((pkgName, { as: alias }, cmd) => {
+  .action((pkgName, { as: alias }) => {
     return run({ installTarget: pkgName, alias })
   })
